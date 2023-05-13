b726977ca1c1bf9ec7b15a2a59a93effca5db606
HESYlaowang <2705542113@qq.com>
2023-05-13 19:26:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const https = require('https');
 
 const signUrl = id => `https://studentapi.lyced.com//student/Sign?StudentId=${id}`
-const flowerUrl = id => `https://studentapi.lyced.com//student/SendFlower?Sender=${id}&Receiver=1194137&FlowerCount=1`
+const flowerUrl = id => `https://studentapi.lyced.com//student/SendFlower?Sender=${id}&Receiver=1194114&FlowerCount=1`
 
 let id = 1000;
 
@@ -10,30 +10,32 @@ function send(i) {
         throw new Error('sss')
     }
     https.get(signUrl(id), res => {
-         let data = '';
+        let data = '';
         // called when a data chunk is received.
         res.on('data', (chunk) => {
-           data += chunk;
+            data += chunk;
         });
-       // called when the complete response is received.
+        // called when the complete response is received.
         res.on('end', () => {
             const v = JSON.parse(data)
             console.log('sign', id, v)
             https.get(flowerUrl(id), res => {
-            let data = '';
+                let data = '';
                 // called when a data chunk is received.
                 res.on('data', (chunk) => {
-                 data += chunk;
+                    data += chunk;
                 });
-             // called when the complete response is received.
-              res.on('end', () => {
+                // called when the complete response is received.
+                res.on('end', () => {
                     const v = JSON.parse(data)
-                   console.log('flwr', id, v)
+                    console.log('flwr', id, v)
+                });
+            }).on('error', e => {
+                console.log(e);
             });
-    });
         });
-    });
-    
+    }).on('error', e => console.log(e));
 }
+
 
 setInterval(() => send(id++), 50)
